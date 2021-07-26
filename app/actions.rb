81aeae2ecb3ce38409dd68933d6f80875eb2e131
naseebakara <naseebakara27@gmail.com>
2021-07-26 05:59:05 +0000

    W3D1b

--- app/actions.rb
+++ app/actions.rb
@@ -45,8 +45,11 @@ get '/' do
       text: "lunchtime! ;)"
     }]
   }
-  [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
+
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+
 erb(:index)
+
 end
 
     
