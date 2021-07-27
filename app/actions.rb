1da26ae37bcc88c7c9cc759a8033db63b1345c07
naseebakara <naseebakara27@gmail.com>
2021-07-27 04:22:23 +0000

    white

--- app/actions.rb
+++ app/actions.rb
@@ -1,13 +1,13 @@
 def humanized_time_ago(time_ago_in_minutes)
-    if time_ago_in_minutes >= 60
-      "#{time_ago_in_minutes / 60} hours ago"
-    else
-      "#{time_ago_in_minutes} minutes ago"
-    end
+  if time_ago_in_minutes >= 60
+    "#{time_ago_in_minutes / 60} hours ago"
+  else
+    "#{time_ago_in_minutes} minutes ago"
   end
+end
 
 get '/' do
-  @finstagram_post_shark = {
+  finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -16,12 +16,11 @@ get '/' do
     comment_count: 1,
     comments: [{
       username: "sharky_j",
-      text: "Out for the long weekend...too embarrassed to show y'all the beach bod!"
-
+      text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
     }]
   }
 
-  @finstagram_post_whale = {
+  finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -29,12 +28,12 @@ get '/' do
     like_count: 0,
     comment_count: 1,
     comments: [{
-    username: "kirk_whalum",
-    text: "#weekendvibes"
-  }]
-}
+      username: "kirk_whalum",
+      text: "#weekendvibes"
+    }]
+  }
 
-  @finstagram_post_marlin = {
+  finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -42,14 +41,9 @@ get '/' do
     like_count: 0,
     comment_count: 1,
     comments: [{
-    username: "marlin_peppa",
-    text: "lunchtime! ;)"
-  }]
-}
-
-  [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
-  erb(:index)
-
+      username: "marlin_peppa",
+      text: "lunchtime! ;)"
+    }]
+  }
+  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
 end
-
-
